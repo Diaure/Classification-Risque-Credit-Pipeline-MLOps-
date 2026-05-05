fa99f558b313ab57cab4bda9a35a6fcb7c7a8739
Aurélie GABU <aurelie.gabu@gmail.com>
2026-05-05 23:54:30 +0200

    Modification des scripts de tests au moment de la vérification

--- App/generate_example.py
+++ App/generate_example.py
@@ -2,11 +2,17 @@
 import pandas as pd
 import json
 import joblib
+import numpy as np
 
 df = joblib.load("./data/app_test_clean_v2.joblib")
 
 # Prendre un individu au hasard
 example = df.sample(1).iloc[0].to_dict()
+for k, v in example.items():
+    if isinstance(v, float) and (np.isnan(v)):
+        example[k] = None
+
+
 
 # Afficher sous format JSON
 print(json.dumps(example, indent=4))
--- App/main.py
+++ App/main.py
@@ -1,41 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import joblib
 import pandas as pd
-from models import ClientFeatures
+from App.models import ClientFeatures
 
 app = FastAPI()
 
-# Charger le pipeline (préprocessing + modèle)
+# Charger le pipeline
 pipe = joblib.load("./BestModel/pipeline_complet.joblib")
 
 # Charger le seuil optimal
 threshold = joblib.load("./BestModel/best_threshold.joblib")
-
-# Validation métier
-df = joblib.load("./data/app_test_clean_v2.joblib")
-if "DAYS_BIRTH" in df.columns:
-    if df["DAYS_BIRTH"].iloc[0] > -18*365 or df["DAYS_BIRTH"].iloc[0] >= 0:
-        raise HTTPException(status_code=422, detail="Âge invalide pour un dossier crédit.")
-
-if "AMT_INCOME_TOTAL" in df.columns:
-    if df["AMT_INCOME_TOTAL"].iloc[0] <= 0:
-        raise HTTPException(status_code=422, detail="Revenu invalide pour un dossier crédit.")
 
 
 @app.post("/predict")
 def predict(data: ClientFeatures):
 
     # Convertir les données brutes en DataFrame
-    df = pd.DataFrame([data.dict()])
+    df = pd.DataFrame([data.model_dump()])   # model_dump = Pydantic v2
 
-    # Prédiction du score (probabilité)
+    # Âge impossible
+    if "DAYS_BIRTH" in df.columns:
+        age = df["DAYS_BIRTH"].iloc[0]
+        if age >= 0 or age > -18 * 365:
+            raise HTTPException(
+                status_code=422,
+                detail="Âge invalide pour un dossier crédit."
+            )
+
+    # Revenu impossible
+    if "AMT_INCOME_TOTAL" in df.columns:
+        income = df["AMT_INCOME_TOTAL"].iloc[0]
+        if income <= 0:
+            raise HTTPException(
+                status_code=422,
+                detail="Revenu invalide pour un dossier crédit."
+            )
+
     score = pipe.predict_proba(df)[0][1]
-
-    # Application du seuil
     decision = "ACCORDÉ" if score < threshold else "REFUSÉ"
 
     return {
-        "Score": float(score),
-        "Décision": decision,
-        "Seuil Optimal": float(threshold)
+        "score": float(score),
+        "decision": decision,
+        "threshold": float(threshold)
     }

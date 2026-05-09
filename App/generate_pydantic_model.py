93c6a39d317694c3f402c0f094dc31f92e42be86
Aurélie GABU <aurelie.gabu@gmail.com>
2026-05-09 22:56:03 +0200

    application files modified

--- App/generate_pydantic_model.py
+++ App/generate_pydantic_model.py
@@ -1,7 +1,23 @@
 import joblib
 import numpy as np
+import re
 
 df = joblib.load("./data/app_test_clean_v2.joblib")
+
+def sanitize(name: str) -> str:
+    """
+    Transforme un nom de colonne en identifiant Python valide.
+    """
+    # Remplacer tout caractère non alphanumérique par un underscore
+    name = re.sub(r'[^0-9a-zA-Z_]', '_', name)
+
+    # Si le nom commence par un chiffre → préfixer
+    if re.match(r'^[0-9]', name):
+        name = f"col_{name}"
+
+    return name
+
+
 example = df.sample(1).iloc[0].to_dict()
 for k, v in example.items():
     if isinstance(v, float) and (np.isnan(v)):
@@ -10,6 +26,8 @@ for k, v in example.items():
 fields = []
 
 for col, dtype in df.dtypes.items():
+    clean_col = sanitize(col)
+
     if dtype == bool or df[col].dropna().isin([0, 1, True, False]).all():
         py_type = "Optional[bool]"
     elif "int" in str(dtype):
@@ -19,7 +37,7 @@ for col, dtype in df.dtypes.items():
     else:
         py_type = "Optional[str]"
 
-    fields.append(f"    {col}: {py_type} = None")
+    fields.append(f"    {clean_col}: {py_type} = None")
 
 
 model_code = f"""

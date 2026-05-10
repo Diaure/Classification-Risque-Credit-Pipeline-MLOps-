eadf144bc1cb210ca71a2bc51b3ebfcdaa834fa2
Aurélie GABU <aurelie.gabu@gmail.com>
2026-05-10 20:43:22 +0200

    init deployment files updated

--- deploy_to_hf.py
+++ deploy_to_hf.py
@@ -10,4 +10,4 @@ api.upload_folder(
     token=os.environ["HF_TOKEN"],
     commit_message="CD: update from GitHub main",
     ignore_patterns=[".git/*", ".github/*", "tests/*", "__pycache__/*", 
-                     "*.pyc", ".venv/*", "venv/*" ])
+                     "*.pyc", ".venv/*", "venv/*", "BestModel/*" ])
